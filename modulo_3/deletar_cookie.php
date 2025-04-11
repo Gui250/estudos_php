da5e305f71dfd6e9a86beb0e9eec909357d017e8
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-04-11 14:22:03 -0300

    estudos sobre cookies

---
+++ modulo_3/deletar_cookie.php
@@ -0,0 +1,5 @@
+<?php 
+
+setcookie('meu_cookie', '', -1); // deletando cookie, colocando uma data no passado
+
+?> 
---
+++ modulo_3/gravar_cookie.php
@@ -0,0 +1,7 @@
+<?php 
+
+// cookie - arquivo de texto atrelado a um dominio
+
+setcookie('meu_cookie', 'valor do meu cookie');
+setcookie('cookie2', 'valor cookie 2'); 
+?> 
